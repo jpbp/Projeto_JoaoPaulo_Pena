8d995d6e8da98e67b84ae6a46bf4b5ee3a741502
jpbp <jpbp@estudante.ufla.br>
2019-04-15 23:42:43 -0300

    criacao das classes A e B

---
+++ classA.py
@@ -0,0 +1,18 @@
+class A(object):
+	def __init__(self,A1,A2):
+		self.A1=A1
+		self.A2=A2
+		
+	def getA1(self):
+		return self.A1
+	def getA2(self):
+		return self.A2
+	def setA1(self,X):
+		self.A1=X
+	def setA2(self,X):
+		self.A2=X
+	def MA1(self):
+		print("MA1")	
+	def MA2(self):
+		print("MA2")
+	
---
+++ classB.py
@@ -0,0 +1,18 @@
+class B(object):
+	def __init__(self,B1,B2):
+		self.B1=B1
+		self.B2=B2
+		
+	def getB1(self):
+		return self.B1
+	def getB2(self):
+		return self.B2
+	def setB1(self,X):
+		self.B1=X
+	def setB2(self,X):
+		self.B2=X
+	def MB1(self):
+		print("MB1")	
+	def MB2(self):
+		print("MB2")
+	
